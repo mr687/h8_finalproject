8604b95e7c27945212e3b42b67683ab58d76b0ad
mr687 <davinomoehdanino@gmail.com>
2021-10-20 19:47:07 +0700

    :tada: added dashboard view

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,106 @@
+@extends('layouts.admin')
+@section('title', __('Dashboard'))
+@section('admin-content')
+    <div class="card">
+      <div class="card-header">
+        <h3 class="card-title">
+          <i class="fas fa-globe"></i>
+          {{ __('Shop Activities') }}
+        </h3>
+      </div>
+      <div class="card-body">
+        <div class="row">
+          <div class="col-sm-6 col-lg-3">
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>Rp 0</h3>
+                <p>{{ __('Total Profit') }}</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-chart-bar"></i>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-3">
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>0</h3>
+                <p>{{ __('Customers') }}</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-users"></i>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-3">
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>0</h3>
+                <p>{{ __('Product Categories') }}</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-tags"></i>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-3">
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>0</h3>
+                <p>{{ __('Total Products') }}</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-shopping-bag"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-6 col-lg-3">
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>0</h3>
+                <p>{{ __('New Orders') }}</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-chart-bar"></i>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-3">
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>0</h3>
+                <p>{{ __('Processed Orders') }}</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-shopping-bag"></i>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-3">
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>0</h3>
+                <p>{{ __('Shipped Orders') }}</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-shopping-bag"></i>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-3">
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>0</h3>
+                <p>{{ __('Orders Done') }}</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-shopping-bag"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+@endsection
